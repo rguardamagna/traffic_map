b23a94abe56b1f45bfc2a11aebf634805f4448e1
lobor3 <lobor3@ubuntu22.04.myguest.virtualbox.org>
2024-01-04 16:40:37 +0100

    v0.4

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Polyline, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Tooltip} from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 import '../styles/map.css'
@@ -12,62 +12,100 @@ import { invertirCoordenadas } from "../utils/utils";
 const MapView = () => {
     
     let trafficFluido = []
-    let trafficDenso,trafficCongestionado,trafficCortado,trafficSinDatos = []
+    let trafficDenso =[]
+    let trafficCongestionado = []
+    let trafficCortado =[]
+    let trafficSinDatos =[]      
     
     let polyline = polylineCreator()  
- /*   
-    let polylineToDraw = []
     
+        
     polyline.forEach(element => {
-        
-        let auxlineToDraw = []
-        
-        element.lines.forEach(coords => {
-            
-            auxlineToDraw.push(invertirCoordenadas(coords))             
-        });
-        polylineToDraw.push(auxlineToDraw)       
-    });
-*/
-
-    polyline.forEach(element => {
-        
-        let auxlineToDraw = []
-        
+        let auxLineToDraw=[]
         switch (element.status) {
-            case "0":
-                element.lines.forEach(coords => {            
-                    auxlineToDraw.push(invertirCoordenadas(coords))       
-                });
-                console.log(auxlineToDraw)
-                let trafficLine = {
-                    'lines': auxlineToDraw.pop(),
-                    'status': element.status
-                };
-                trafficFluido.push(trafficLine)
+            case 0:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficFluido.push(auxLineToDraw)
                 break;
-            case "1":
+            case 1:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficDenso.push(auxLineToDraw)
                 break;
-            case "2":
+            case 2:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficCongestionado.push(auxLineToDraw)
                 break;            
-            case "3":
+            case 3:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficCortado.push(auxLineToDraw)
+                break;           
+            case 5:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficFluido.push(auxLineToDraw)
                 break;
-            case "4":
+            case 6:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficDenso.push(auxLineToDraw)
                 break;
-            case "5":
+            case 7:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficCongestionado.push(auxLineToDraw)
+                break;            
+            case 8:
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficCortado.push(auxLineToDraw)
                 break;
-
+            
             default:
-                break;
+                element.lines.forEach(coords => {
+                    auxLineToDraw.push(invertirCoordenadas(coords))
+                });                
+                trafficSinDatos.push(auxLineToDraw)
+                break;            
         }                
     });
-    console.log(trafficFluido)
-    const limeOptions = { color: 'yellow' }
-    //<Polyline pathOptions={limeOptions} positions={polylineToDraw} />
+    
+    const limeOptions = { color: 'lime' }    
+    const orangeOptions = { color: 'orange' }
+    const redOptions = { color: 'red' }
+    const blackOptions = { color: 'black' }
+    const grayOptions = { color: 'gray' }
+    
     return (
         <div>
             <MapContainer center={[39.46746287691412,-0.3778836267835896]} zoom={13} className="mapa">
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                <Polyline pathOptions={limeOptions} positions={trafficFluido} >
+                    <Tooltip sticky>Estado del tráfico: Fluido</Tooltip>
+                </Polyline>                  
+                <Polyline pathOptions={orangeOptions} positions={trafficDenso}>
+                    <Tooltip sticky>Estado del tráfico: Denso</Tooltip>
+                </Polyline>
+                <Polyline pathOptions={redOptions} positions={trafficCongestionado}>
+                    <Tooltip sticky>Estado del tráfico: Congestionado</Tooltip>
+                </Polyline>
+                <Polyline pathOptions={blackOptions} positions={trafficCortado}>
+                    <Tooltip sticky>Estado del tráfico: Cortado</Tooltip>    
+                </Polyline>
+                <Polyline pathOptions={grayOptions} positions={trafficSinDatos}>
+                    <Tooltip sticky>Estado del tráfico: Sin datos</Tooltip>
+                </Polyline>
             </MapContainer>            
         </div>
     )
--- src/trafficStatus.js
+++ src/trafficStatus.js
@@ -1,17 +1,27 @@
 import { useFetch } from "./useFetch"
 
-const url = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/intensitat-transit-trams-intensidad-trafico-tramos/records?limit=-1    '
+const urls = [
+    'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/estat-transit-temps-real-estado-trafico-tiempo-real/records?where=idtramo%3C100&limit=-1',
+    'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/estat-transit-temps-real-estado-trafico-tiempo-real/records?where=idtramo%3E%3D100%20and%20idtramo%3C200&limit=-1',
+    'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/estat-transit-temps-real-estado-trafico-tiempo-real/records?where=idtramo%3E%3D200%20and%20idtramo%3C300&limit=-1',
+    'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/estat-transit-temps-real-estado-trafico-tiempo-real/records?where=idtramo%3E%3D300%20and%20idtramo%3C400&limit=-1',
+    'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/estat-transit-temps-real-estado-trafico-tiempo-real/records?where=idtramo%3E%3D400%20and%20idtramo%3C500&limit=-1'
+    ]
 
 export function polylineCreator() {
     
-    const {data} = useFetch(url)
     let polylineToDraw=[]
+    
+    const {data} = useFetch(urls)
+    
     if (data != null)    
         {
-            data.results.forEach(element => {
+            data.forEach(element => {
                 let newFeature = {
                     'lines':element.geo_shape.geometry.coordinates,
-                    'status':element.estado
+                    'status':element.estado,
+                    'id':element.idtramo,
+                    'address':element.denominacion
                 }
                 polylineToDraw.push(newFeature)
             
